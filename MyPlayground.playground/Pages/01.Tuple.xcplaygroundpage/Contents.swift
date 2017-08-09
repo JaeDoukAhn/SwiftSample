49185958abe8a75ae48869115b0e3e2f7d35ad38
JD Ahn <jdahn1914@gmail.com>
2017-08-09 15:12:04 +0900

    테스트

---
+++ MyPlayground.playground/Pages/01.Tuple.xcplaygroundpage/Contents.swift
@@ -0,0 +1,16 @@
+//Collection Type
+let anyVal = ("홍길동", 23, 170.5, "010-555-5555")
+anyVal.0
+anyVal.1
+anyVal.2
+anyVal.3
+
+
+let person1:(String, Int, String) = ("김철수", 22, "010-555-5555")
+
+let (name, age, phoneNumber) = person1
+name
+age
+phoneNumber
+
+print("AAAA")
